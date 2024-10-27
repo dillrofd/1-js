bd502e3307f3908d19d6d288b31c939f0bf253e9
dillrofd <148703484+dillrofd@users.noreply.github.com>
2024-10-27 21:59:48 +0200

    Create try.js

---
+++ js/try.js
@@ -0,0 +1,33 @@
+let age = 16;
+
+console.log(age);
+
+
+const name = "Yuna";
+
+console.log(name);
+
+
+
+let isStudent = true;
+
+console.log(isStudent);
+
+
+
+let myString ="Who needs a wide, bright path that's already crowded? I'll walk my crooked path until it's dark!";
+ 
+console.log(myString);
+
+
+let myNumber = 13;
+
+myNumber = myNumber + 10;
+
+console.log(myNumber);
+
+
+
+const myNull = null;
+
+console.log(myNull);
