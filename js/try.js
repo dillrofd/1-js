c0ababd9ceed5f0a21b0b1c304941eeb13ed969e
dillrofd <148703484+dillrofd@users.noreply.github.com>
2024-11-04 18:14:32 +0200

    Update try.js

--- js/try.js
+++ js/try.js
@@ -31,3 +31,26 @@ console.log(myNumber);
 const myNull = null;
 
 console.log(myNull);
+
+const userName = prompt (" Будь ласка, введіть ваше ім'я ")
+console.log(typeof userName)
+alert ( `Привіт, ${userName}!` )
+
+
+const userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log(typeof userAnswer);
+if (userAnswer === true){ 
+alert ("Дякую за підтвердження!");
+} else {
+    alert ("Дію відмінено!") ;
+}
+
+
+alert (" Увага! Дія може бути небезпечною. ");
+const message = confirm ("Ви впевнені що хочете продовжити?");
+
+if (message === true){ 
+    alert ("Дякую за підтвердження!");
+    } else {
+        alert ("Дію відмінено!") ;
+    }
